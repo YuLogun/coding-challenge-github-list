aee14f10f964e9b11f5f8cbf12fb7470789d1d93
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-05-31 10:12:55 +0400

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
-import UserName from "./components/UserName";
-import ReadMeMarkup from "./components/ReadMeMarkup";
+
+import "./styles/LoadingIcon.css";
+
+const Home = React.lazy(() => import("./components/Home"));
+const UserName = React.lazy(() => import("./components/UserName"));
+const ReadMeMarkup = React.lazy(() => import("./components/ReadMeMarkup"));
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/:userName" component={UserName} />
-        <Route
-          exact
-          path="/:userName/:repositoryName"
-          component={ReadMeMarkup}
-        />
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="loader-container">
+            <div className="lds-ripple">
+              <div />
+              <div />
+            </div>
+          </div>
+        }
+      >
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/:userName" component={UserName} />
+          <Route
+            exact
+            path="/:userName/:repositoryName"
+            component={ReadMeMarkup}
+          />
+        </Switch>
+      </Suspense>
     </div>
   );
 }
